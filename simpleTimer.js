a0cbbef18d2054f01d9f0b0a24a0e03863a7f682
James Ball <jamie.j.ball@gmail.com>
2022-05-10 13:36:40 -0700

    added a super simple countdown timer

--- simpleTimer.js
+++ simpleTimer.js
@@ -11,3 +11,16 @@ const timer = () => {
   }
 };
 timer();
+
+// clearInterval(a);
+
+const timer2 = () => {
+  let count = 5;
+
+  const a = setInterval(() => {
+    console.log(count);
+    count--;
+    if (count === 0) clearInterval(a);
+  }, 1000);
+};
+timer2();
