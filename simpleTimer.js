2605a6428f55daa5305490d13563efdf7b248c86
James Ball <jamie.j.ball@gmail.com>
2022-05-10 08:49:57 -0700

    created a simple timer

--- simpleTimer.js
+++ simpleTimer.js
@@ -1,1 +1,13 @@
-h;
+const timer = () => {
+  const args = process.argv.slice(2);
+  // const args = [arg1, arg2];
+  for (let num of args) {
+    num = Number(num);
+    if (isNaN(num) || num <= 0) continue;
+    num = num * 1000;
+    setTimeout(() => {
+      process.stdout.write(".");
+    }, num);
+  }
+};
+timer();
